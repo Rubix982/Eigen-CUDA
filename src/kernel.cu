d18faf11525a49b746ef2381052e87188cff26bb
Saif Ul Islam <saifulislam84210@gmail.com>
2020-11-14 19:58:07 +0500

    basic global function

--- src/kernel.cu
+++ src/kernel.cu
@@ -1,16 +1,31 @@
-#include "matrix.hpp"
 #include "kernel.cuh"
+#include "matrix.hpp"
 #include <cstdio>
+#include <cuda_runtime.h>
 
-#define N 128
+#define N 256
 #define TPB 32
+#define MATRIX_DIMENSIONS 256
 
 int main(int argc, char const *argv[]) {
-  launchKernel<<<N/TPB, TPB>>>();
+  
+  // Declaring MatA, MatB
+  EigenCUDA::Matrix MatA(MATRIX_DIMENSIONS, MATRIX_DIMENSIONS),
+      MatB(MATRIX_DIMENSIONS, MATRIX_DIMENSIONS);
 
-  EigenCUDA::Matrix MatA(4, 4);
-  MatA.init__MatWithUniformDistribution(1, 100, true);
-  MatA.printMatrix();
+  // Intializing Matricies
+  MatA.init__MatWithUniformDistribution(1, 100, false),
+      MatB.init__MatWithUniformDistribution(1, 199, false);
+
+  double *finalMat = NULL; 
+
+  cudaMalloc(&finalMat, N * sizeof(double));
+
+  dim3 gridSize(N / (2*TPB), N / (2*TPB), 1);
+  dim3 blockSize(TPB / 2, TPB / 2, 1);
+  launchKernel<<<gridSize, blockSize>>>(MatA, MatB, finalMat, MATRIX_DIMENSIONS, N);
+
+  cudaFree(finalMat);
 
   return 0;
 }
--- src/kernel.cuh
+++ src/kernel.cuh
@@ -1,6 +1,34 @@
 #ifndef MY_KERNEL_AUX_FUNCTIONS
 #define MY_KERNEL_AUX_FUNCTIONS
 
-__global__ void launchKernel(void) { printf("Hello, world!\n"); }
+#include "matrix.hpp"
+#include <stdio.h>
+#include <cuda_runtime.h>
+
+#define ull unsigned long long
+
+__global__ void launchKernel(const EigenCUDA::Matrix &MatA,
+                             const EigenCUDA::Matrix &MatB, double *finalMat,
+                             ull MATRIX_DIMENSIONS, int threads) {
+  // const int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+  ull nrows = MATRIX_DIMENSIONS, ncols = MATRIX_DIMENSIONS;
+
+  double *rowArray = NULL, *colArray = NULL;
+  cudaMalloc(&rowArray, MATRIX_DIMENSIONS * sizeof(double));
+  cudaMalloc(&colArray, MATRIX_DIMENSIONS * sizeof(double));
+
+  for (ull i = 0; i < nrows; ++i) {
+    for (ull j = 0; j < ncols; ++j) {
+      // rowArray = getRowFromMatrix(MatA, i);
+      // colArray = getColFromMatrix(MatB, i);
+    }
+  }
+
+  cudaFree(rowArray);
+  cudaFree(colArray);
+
+  return ;
+}
 
 #endif
